4c2c0c802960a51389d2f7a5452c06dc90cfccc4
Kevin <gr20060513@gmail.com>
2016-10-18 14:19:48 +0800

    Initial Commit

---
+++ GDChartSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDChartSample
+//
+//  Created by ctitv on 2016/10/18.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
